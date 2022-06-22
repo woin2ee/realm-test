4ba314c8c7d6ec8bcb34f7850946e229a00523fb
Jaewon <woin2ee@gmail.com>
2022-06-22 22:20:18 +0900

    Realm 의 observe 를 이용하여 DB 와 바인딩

--- RealmTest/ViewController.swift
+++ RealmTest/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     }
     
     let realm = try! Realm()
+    var token: NotificationToken?
     
     var items: [Item] = []
     
@@ -26,23 +27,36 @@ class ViewController: UIViewController {
         
         print(realm.configuration.fileURL!)
         
-        fetchItems()
+        observeItems()
     }
     
     @IBAction func clickAddButton(_ sender: Any) {
         try! realm.write {
             realm.add(Item(content: "Content"))
         }
-        fetchItems()
-        reloadTable()
     }
     
-    func fetchItems() {
+    func observeItems() {
         let items = realm.objects(Item.self)
-        self.items = items.map { $0 }
+        
+        token = items.observe { changes in
+            switch changes {
+            case .initial(let items):
+                self.reloadTable(to: items)
+            case .update(let items, let deletions, let insertions, let modifications):
+                self.reloadTable(to: items)
+                
+                print("Deleted indices: ", deletions)
+                print("Inserted indices: ", insertions)
+                print("Modified modifications: ", modifications)
+            case .error(let error):
+                fatalError(error.localizedDescription)
+            }
+        }
     }
     
-    func reloadTable() {
+    func reloadTable(to items: Results<Item>) {
+        self.items = items.map { $0 }
         tableView.reloadData()
     }
 }
